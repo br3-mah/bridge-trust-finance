80217d2098dc6bfb0fdcd42b785aafadfcf33eb7
Br3-mah <nyeleti.bremah@gmail.com>
2023-02-15 01:00:08 +0200

    KYC Completion 2 | real admin email

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,11 +16,11 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::create([
-            'fname' => 'I am',
-            'lname' => 'Admin',
-            // 'email' => 'admin@bridgetrustfinance.co.zm',
-            'email' => 'roadoc404@gmail.com',
-            'password' => bcrypt('bridge4u'),
+            'fname' => 'Brige Trust',
+            'lname' => 'Finance',
+            'email' => 'admin@bridgetrustfinance.com',
+            // 'email' => 'admin@gmail.com',
+            'password' => bcrypt('bridge.@2023'),
         ])->assignRole('admin');
 
         User::factory(2)->create();
