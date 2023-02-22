4397ab7276065a1bf40784192ec530e43b100ce0
Br3-mah <nyeleti.bremah@gmail.com>
2023-02-23 00:55:56 +0200

    Error checking

--- app/Http/Controllers/LoanApplicationController.php
+++ app/Http/Controllers/LoanApplicationController.php
@@ -296,7 +296,7 @@ class LoanApplicationController extends Controller
             'fname'=> $user->fname,
             'email'=> $user->email,
             'amount'=> $form['amount'],
-            'phone'=> $user->phone,
+            'phone'=> preg_replace("/[^a-zA-Z0-9]/", "", $user->phone),
             'gender'=> $user->gender,
             'type'=> $form['type'],
             'repayment_plan'=> $form['repayment_plan'],
