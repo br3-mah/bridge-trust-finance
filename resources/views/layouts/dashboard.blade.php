326ada0e48a0a3e8703b92020612c50369a9d90b
br3-mah <nyeleti.bremah@gmail.com>
2023-03-10 22:18:58 +0200

    stable commit fixing assets

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -86,6 +86,8 @@
 			</div>
 		</div>
 	</div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
 	<!-- Required public/vendors -->
 	<script src="{{ asset('public/vendor/global/global.min.js') }}" ></script>
 	<script src="{{ asset('public/vendor/chart.js/Chart.bundle.min.js') }}" ></script>
@@ -151,7 +153,6 @@
 	<script src="{{ asset('public/js/deznav-2-init.js') }}" ></script>
 	<script src="{{ asset('public/js/custom.min.js') }}" ></script>
 	<script src="{{ asset('public/js/demo-2.js') }}" ></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 	<script language="javascript">
 		$(function() {
 			$('#datetimepicker').datetimepicker({
