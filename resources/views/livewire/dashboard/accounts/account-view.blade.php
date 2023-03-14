011b1fae7e1c086d2f69d069a558d10a736a9f2f
br3-mah <nyeleti.bremah@gmail.com>
2023-03-14 12:26:28 +0200

    Account detail profile picture size

--- resources/views/livewire/dashboard/accounts/account-view.blade.php
+++ resources/views/livewire/dashboard/accounts/account-view.blade.php
@@ -18,7 +18,7 @@
                                         <span>{{ $data->name[0] }}</span>
                                     @endif
                                 @else
-                                    <img class="w-20 h-20 img-fluid rounded-circle bg-primary" src="{{ 'public/'.Storage::url($data->profile_photo_path) }}" />
+                                    <img class="w-30 h-40 img-fluid rounded-circle bg-primary" src="{{ 'public/'.Storage::url($data->profile_photo_path) }}" />
                                 @endif
                             </div>
                             <div class="profile-details">
